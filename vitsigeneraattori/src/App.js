b229f6f1394a7181b64efe86961148861f5d6124
Jere Mäennenä <jere.maennena@gmail.com>
2024-02-13 22:51:07 +0200

    Added a new div, included an emoji and made it spin! 😄🔄 Added an emoji, button, and joke-texts inside this box. Small tries and fixes.

--- vitsigeneraattori/src/App.js
+++ vitsigeneraattori/src/App.js
@@ -1,48 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const [setup, setSetup] = useState("");
   const [delivery, setDelivery] = useState("");
   const [joke, setJoke] = useState("");
 
-
   useEffect(() => {
-    const haeVitsiAlussa = async () => {
+    const getJokeFirst = async () => {
       try {
-        const response = await fetch('https://v2.jokeapi.dev/joke/Any?safe-mode');
+        const response = await fetch(
+          "https://v2.jokeapi.dev/joke/Any?safe-mode"
+        );
         const data = await response.json();
         console.log(data);
         if (data.type === "twopart") {
           setSetup(data.setup);
           setDelivery(data.delivery);
-          setJoke('');
+          setJoke("");
         } else if (data.type === "single") {
           setJoke(data.joke);
-          setSetup('');
-          setDelivery('');
+          setSetup("");
+          setDelivery("");
         }
       } catch (error) {
         console.log(error);
       }
     };
-    haeVitsiAlussa();
+    getJokeFirst();
   }, []);
 
-  const haeVitsi = async () => {
+  const getJoke = async () => {
     try {
-      const response = await fetch('https://v2.jokeapi.dev/joke/Any?safe-mode');
+      const response = await fetch("https://v2.jokeapi.dev/joke/Any?safe-mode");
       const data = await response.json();
       console.log(data);
       if (data.type === "twopart") {
         setSetup(data.setup);
         setDelivery(data.delivery);
-        setJoke('');
+        setJoke("");
       } else if (data.type === "single") {
         setJoke(data.joke);
-        setSetup('');
-        setDelivery('');
+        setSetup("");
+        setDelivery("");
       }
     } catch (error) {
       console.log(error);
@@ -52,19 +52,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button style={{
-          backgroundColor: '#00bcd4',
-          color: 'white',
-          border: 'none',
-          padding: '10px',
-          borderRadius: '5px',
-          fontSize: '20px',
-          cursor: 'pointer'
-        }} onClick={haeVitsi}>Hae vitsi</button>
-        <div className='teksti'>
-          <h1 className='teksti' style={{ color: '#00bcd4' }}>{setup}</h1>
-          <h1 className='teksti'>{delivery}</h1>
-          <h1 className='teksti'>{joke}</h1>
+        <div className="wrapper">
+          <span>&#129315;</span>
+          <p>
+            <div className="teksti">
+              <h1 className="teksti" style={{ color: "#00bcd4" }}>
+                {setup}
+              </h1>
+              <h1 className="teksti">{delivery}</h1>
+              <h1 className="teksti">{joke}</h1>
+            </div>
+          </p>
+          <button
+            style={{
+              backgroundColor: "#fab22e",
+              color: "black",
+              border: "none",
+              padding: "10px",
+              borderRadius: "5px",
+              fontSize: "20px",
+              cursor: "pointer",
+            }}
+            onClick={getJoke}>
+            Get Joke
+          </button>
         </div>
       </header>
     </div>
