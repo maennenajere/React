90a63d5c8850d6f0124046eab4a8148e4ea62490
Jere Mäennenä <jere.maennena@gmail.com>
2024-10-24 23:46:48 +0300

    Add login route

--- todo/server/routes/userRouter.js
+++ todo/server/routes/userRouter.js
@@ -1,8 +1,9 @@
-import { pool } from '../db.js';
+import { pool } from '../helper/db.js';
 import { Router } from 'express';
 import { hash, compare } from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { sign } from jwt;
+
+const { sign } = jwt;
 
 const router = Router();
 
@@ -20,6 +21,31 @@ router.post('/register', (req, res, next) => {
             return next(error);
         }
     });
+});
+
+router.post('/login', (req, res, next) => {
+    const invalid_message = 'Invalid credentials';
+    try {
+        pool.query('SELECT * FROM account WHERE email = $1',
+            [req.body.email],
+            (error, result) => {
+                if (error) return next(error);
+                if (result.rowCount === 0) return next(new Error(invalid_message));
+                compare(req.body.password, result.rows[0].password, (error, match) => {
+                    if (error) return next(error);
+                    if (!match) return next(new Error(invalid_message));
+                    const token = sign({ user: req.body.email }, process.env.JWT_SECRET_KEY);
+                    const user = result.rows[0];
+                    return res.status(200).json({
+                        id: user.id,
+                        email: user.email,
+                        token: token
+                    });
+                });
+            });
+    } catch (error) {
+        return next(error);
+    }
 });
 
 export default router;
