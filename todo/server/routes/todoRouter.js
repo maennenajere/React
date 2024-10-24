3d67935d10e19c0d1dc9a80c762556f7558340d9
Jere Mäennenä <jere.maennena@gmail.com>
2024-10-24 23:47:50 +0300

    Add task routes with authentication

--- todo/server/routes/todoRouter.js
+++ todo/server/routes/todoRouter.js
@@ -1,29 +1,27 @@
 import { pool } from '../helper/db.js';
 import { Router } from 'express';
+import { emptyOrRows } from '../helper/utils.js';
+import { auth } from '../helper/auth.js';
 
 const router = Router();
 
-router.get('/',(req,res) => {
+router.get('/',(req,res,next) => {
     pool.query('SELECT * FROM task', (error, result) => {
-        if (error) {
-            return res.status(500).json({error: error.message});
-        }
-        return res.status(200).json(result.rows);
+        if (error) return next(error);
+        return res.status(200).json(emptyOrRows(result));
     });
 });
 
-router.post('/create',(req,res) => {
+router.post('/create',auth,(req,res, next) => {
     pool.query('INSERT INTO task (description) VALUES ($1) RETURNING *',
         [req.body.description],
         (error,result) => {
-            if (error) {
-                return res.status(500).json({error: error.message});
-            }
-            return res.status(200).json({id: result.rows[0].id});
-        });
+        if (error) return next(error);
+        return res.status(200).json({id: result.rows[0].id});
+    });
 });
 
-router.delete('/delete/:id',(req,res) => {
+router.delete('/delete/:id',auth,(req,res, next) => {
     const id = parseInt(req.params.id);
     pool.query('DELETE FROM task WHERE id = $1',
         [id],
