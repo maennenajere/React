05a7c2bbbe96938543d521317add04d84a234cbd
Jere Mäennenä <jere.maennena@gmail.com>
2024-10-29 20:23:00 +0200

    Small changes

--- todo/server/helper/test.js
+++ todo/server/helper/test.js
@@ -1,20 +1,21 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { pool } from './db.js';
-import { sign } from 'jsonwebtoken';
+import pkg from 'jsonwebtoken';
 import { hash } from 'bcrypt';
 
-const __dirname = import.meta.dirname;
+const { sign } = pkg;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const initializeTestDb = () => {
-    const sql = fs.readFileSync(path.resolve(__dirname, "../todo.sql"), "utf8");
+    const sql = fs.readFileSync(path.resolve(__dirname, "../SQL/todo.sql"), "utf8");
     pool.query(sql);
 };
 
 const insertTestUser = (email, password) => {
     hash(password, 10, (error, hashedPassword) => {
-        pool.query('INSERT INTO account (email, password) VALUES ($1, $2)',
-            [email, hashedPassword]);
+        pool.query('INSERT INTO account (email, password) VALUES ($1, $2)', [email, hashedPassword]);
     });
 };
 
