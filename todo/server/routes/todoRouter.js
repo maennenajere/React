1328211d77b6bd07604a636c120bb8ea2def9910
Jere Mäennenä <jere.maennena@gmail.com>
2024-10-29 20:23:11 +0200

    Small changes

--- todo/server/routes/todoRouter.js
+++ todo/server/routes/todoRouter.js
@@ -1,27 +1,32 @@
 import { pool } from '../helper/db.js';
 import { Router } from 'express';
-import { emptyOrRows } from '../helper/utils.js';
+//import { emptyOrRows } from '../helper/utils.js';
 import { auth } from '../helper/auth.js';
+import { getTasks, postTask, deleteTaskById } from '../controllers/TaskController.js';
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', getTasks);
+router.post('/create', auth, postTask);
+router.delete('/delete/:id', auth, deleteTaskById);
+
+/*router.get('/', (req, res, next) => {
     pool.query('SELECT * FROM task', (error, result) => {
         if (error) return next(error);
         return res.status(200).json(emptyOrRows(result));
     });
-});
+});*/
 
-router.post('/create', auth, (req, res, next) => {
+/*router.post('/create', auth, (req, res, next) => {
     pool.query('INSERT INTO task (description) VALUES ($1) RETURNING *',
         [req.body.description],
         (error, result) => {
             if (error) return next(error);
             return res.status(200).json({ id: result.rows[0].id });
         });
-});
+});*/
 
-router.delete('/delete/:id', auth, (req, res, next) => {
+/*router.delete('/delete/:id', auth, (req, res, next) => {
     const id = parseInt(req.params.id);
     pool.query('DELETE FROM task WHERE id = $1',
         [id],
@@ -31,6 +36,6 @@ router.delete('/delete/:id', auth, (req, res, next) => {
             }
             return res.status(200).json({ id: id });
         });
-});
+});*/
 
 export default router;
